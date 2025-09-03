27409566b581e5c3a3e23ade4bc39139f5589405
MrNDN-24 <ducnhan279notionktkt279@gmail.com>
2025-09-03 15:50:23 +0700

    Login/JWT

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -5,9 +5,18 @@ import { AuthController } from './auth.controller';
 import { User } from '@/modules/users/entities/user.entity';
 import { Author } from '../authors/entities/author.entity';
 import { SharedModule } from '../shared/shared.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_EXPIRATION } from '@/common/constants/jwt.constant';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Author]), SharedModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: JWT_EXPIRATION }, // access token hết hạn sau 15 phút
+    }),
+    TypeOrmModule.forFeature([User, Author]),
+    SharedModule,
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
